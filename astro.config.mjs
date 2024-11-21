fb53e0c6469180f033207d8dfc452b5316f78781
jhonatanramires <74281454+jhonatanramires@users.noreply.github.com>
2024-11-21 20:34:16 +0000

    Optimus Prime

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,41 +6,36 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+import netlify from '@astrojs/netlify';
 import markdoc from "@astrojs/markdoc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-import remarkCodeTitles from 'remark-code-titles'
+import remarkCodeTitles from 'remark-code-titles';
 import decapCmsOauth from "astro-decap-cms-oauth";
 
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
   output: 'server',
-  site: 'https://astro-ink.vercel.app', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  site: 'https://astro-ink.vercel.app',
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   server: {
     // port: 4321, // The port to run the dev server on.
   },
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
-      theme: 'css-variables',
+      theme: 'css-variables'
     },
-    remarkPlugins: [
-      remarkCodeTitles
-    ]
+    remarkPlugins: [remarkCodeTitles]
   },
-  integrations: [
-    mdx(), 
-    markdoc(),
-    svelte(), 
-    tailwind({
-      applyBaseStyles: false,
-    }), 
-    sitemap(),
-    decapCmsOauth()
-  ],
+  integrations: [mdx(), markdoc(), svelte(), tailwind({
+    applyBaseStyles: false
+  }), sitemap(), decapCmsOauth()],
   vite: {
     plugins: [],
     resolve: {
@@ -52,5 +47,5 @@ export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
       allowNodeBuiltins: true
     }
   },
-  adapter: vercel()
+  adapter: netlify()
 });
